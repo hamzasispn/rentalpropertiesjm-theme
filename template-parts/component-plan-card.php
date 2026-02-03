0318481f1dfef14b55715b5009139484c31082c6
hamza sispn <talha.sispn@gmail.com>
2026-02-03 06:04:54 +0500

    Landing Page Responsive Completed and Updates

--- template-parts/component-plan-card.php
+++ template-parts/component-plan-card.php
@@ -29,45 +29,45 @@ foreach ($plans_data as $plan) {
 
 
     <div
-            class="px-[0.833vw] <?= $is_best_seller ? 'bg-[var(--primary-color)]' : 'bg-white'; ?> pb-6 pt-[2.2vw] relative overflow-hidden rounded-[16px] shadow-xl flex flex-col gap-4 <?php echo $is_current ? 'border-2 border-blue-600' : ''; ?>">
+            class="!mt-[0px] !mx-[0px] !mb-[0px] px-[6.833vw] md:px-[0.833vw] md:w-auto w-full <?= $is_best_seller ? 'bg-[var(--primary-color)]' : 'bg-white'; ?> pb-6 pt-[12.2vw] md:pt-[2.2vw] relative overflow-hidden shadow-lg rounded-[16px] flex flex-col gap-4 <?php echo $is_current ? 'border-2 border-blue-600' : ''; ?>">
         <?php if ($is_best_seller): ?>
-            <div class="bg-white text-[var(--primary-color)] absolute rounded-l-full right-[0px] top-[15px] py-2 px-4 text-[1.042vw] font-semibold font-inter text-center uppercase">
+            <div class="bg-white text-[var(--primary-color)] absolute rounded-l-full right-[0px] top-[15px] py-2 px-4 text-[3.042vw] md:text-[1.042vw] font-semibold font-inter text-center uppercase">
                 Best Seller
             </div>
         <?php endif; ?>
         <div class="flex justify-between items-start mb-2">
-            <h3 class="text-[1.875vw] font-bold w-[70%] leading-[1] <?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?>"><?php echo esc_html($plan['name']); ?></h3>
+            <h3 class="text-[6.875vw] md:text-[1.875vw] font-bold w-[70%] leading-[1] <?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?>"><?php echo esc_html($plan['name']); ?></h3>
             <?php if ($is_current): ?>
                 <span class="px-3 py-1 bg-blue-100 text-[var(--primary-color)] text-xs font-semibold rounded ">Current
                                     Plan</span>
             <?php endif; ?>
 
         </div>
-        <p class="<?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?> text-[0.833vw] font-inter">Individual homeowners listing a
+        <p class="<?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?> text-[3.833vw] md:text-[0.833vw] font-inter">Individual homeowners listing a
             single property</p>
 
-        <h6 class="<?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?> text-[2.5vw] font-bold font-inter">$<?= $plan['price']; ?>
+        <h6 class="<?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?> text-[9.5vw] md:text-[2.5vw] font-bold font-inter">$<?= $plan['price']; ?>
             <span
-                    class="text-[0.833vw] font-light">/ <?= $plan['billing_cycle'] ?></span>
+                    class="text-[3.765vw] md:text-[0.833vw] font-light">/ <?= $plan['billing_cycle'] ?></span>
         </h6>
 
-        <ul class=" flex flex-col gap-5 <?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?> text-[0.833vw]">
-            <li class="font-bold font-inter text-[0.99vw]">
+        <ul class=" flex flex-col gap-5 <?= $is_best_seller ? 'text-white' : 'text-[#1A1A1A]'; ?>">
+            <li class="font-bold font-inter text-[4.67vw] md:text-[0.99vw]">
                 Listings Included :
                 <span class="font-light">
                     <?= $plan['max_properties'] == 1 ? '1 property' : 'Up to ' . esc_html($plan['max_properties']) . ' properties'; ?>
                  </span>
             </li>
 
-            <li class="font-bold font-inter text-[0.99vw]">Featured Listing : <span
+            <li class="font-bold font-inter text-[4.67vw] md:text-[0.99vw]">Featured Listing : <span
                         class="font-light"><?= $plan['featured_limit'] == 1 ? '1 property' : 'Up to ' . esc_html($plan['featured_limit']) . ' properties'; ?></span></li>
-            <li class="font-bold font-inter text-[0.99vw]">Advanced Analytics : <span
+            <li class="font-bold font-inter text-[4.67vw] md:text-[0.99vw]">Advanced Analytics : <span
                         class="font-light"><?php echo $plan['analytics'] ? 'Available' : 'Not Available'; ?></span></li>
             <?php if ($plan['features']): ?>
-                <li class="font-bold font-inter text-[0.99vw] flex gap-6">Features
+                <li class="font-bold font-inter text-[4.67vw] md:text-[0.99vw] flex gap-6">Features
                     <ul class="list-disc">
                         <?php foreach ($plan['features'] as $feature): ?>
-                            <li class="font-medium text-[0.729vw]"><?= esc_html($feature); ?></li>
+                            <li class="font-medium text-[3.729vw] md:text-[0.729vw]"><?= esc_html($feature); ?></li>
                         <?php endforeach; ?>
                     </ul>
                 </li>
@@ -91,7 +91,7 @@ foreach ($plans_data as $plan) {
 
             <?php else: ?>
                 <a href="<?= home_url() . '/login'; ?>"
-                   class="block px-4 py-2 font-semibold font-inter <?= $is_best_seller ? 'bg-white text-[var(--primary-color)] hover:bg-white/80' : 'bg-[var(--primary-color)] text-white hover:bg-blue-700' ?> rounded-lg transition text-[0.938vw] w-full text-center">
+                   class="block px-4 py-2 font-semibold font-inter <?= $is_best_seller ? 'bg-white text-[var(--primary-color)] hover:bg-white/80' : 'bg-[var(--primary-color)] text-white hover:bg-blue-700' ?> rounded-lg transition text-[3.765vw] md:text-[0.938vw] w-full text-center">
                     Get
                     Started
                 </a>
--- template-parts/section-hero.php
+++ template-parts/section-hero.php
@@ -10,22 +10,22 @@ $heroExc = get_field('hero_sec_excerpt');
 
 <section class="heroSec h-screen flex items-center bg-cover bg-center relative"
     style="background-image: url('<?= esc_url($bgImg['url']) ?>');">
-    <div class="absolute bg-white rounded-tl-[24px] bottom-0 right-0 w-[30.719vw] h-[8.75vw]"></div>
+    <div class="absolute bg-white rounded-tl-[24px] bottom-0 right-0 w-[30.719vw] md:h-[8.75vw] h-[25vw]"></div>
     <div class="w-[80%] mx-auto flex items-center justify-between">
-        <div class="heroContent max-w-[80%] text-[var(--text-secondary-color)]">
-            <h1 class="text-[3.563vw] font-bold leading-none"><?= esc_html($heroTitle); ?>
-                <h2 class="text-[2.7vw] font-semibold"><?= esc_html($heroSubtitle); ?></h2>
+        <div class="heroContent w-full md:max-w-[80%] text-[var(--text-secondary-color)]">
+            <h1 class="text-[10.353vw] md:text-[3.563vw] font-bold leading-none"><?= esc_html($heroTitle); ?>
+                <h2 class="text-[8.176vw] md:text-[2.7vw] font-semibold leading-none md:leading-normal"><?= esc_html($heroSubtitle); ?></h2>
             </h1>
-            <div class="text-[0.833vw] !mb-[2.083vw] max-w-[34.063vw]"><?= $heroDesc ?></div>
-            <div class="flex items-center gap-[1.25vw] mb-[2.5vw]">
+            <div class="text-[3.765vw] md:text-[0.833vw] !mb-[2.083vw] md:max-w-[34.063vw] max-w-full my-[4vw] md:my-0"><?= $heroDesc ?></div>
+            <div class="flex md:flex-row flex-col items-center gap-[3.765vw] md:gap-[1.25vw] mb-[2.5vw]">
                 <?php if ($ctaOne): ?>
-                    <a href="<?= esc_url($ctaOne['url']); ?>" class="btn-primary"><?= esc_html($ctaOne['title']); ?></a>
+                    <a href="<?= esc_url($ctaOne['url']); ?>" class="btn-primary block md:inline-block md:w-auto w-full text-center"><?= esc_html($ctaOne['title']); ?></a>
                 <?php endif; ?>
                 <?php if ($ctaTwo): ?>
-                    <a href="<?= esc_url($ctaTwo['url']); ?>" class="btn-secondary"><?= esc_html($ctaTwo['title']); ?></a>
+                    <a href="<?= esc_url($ctaTwo['url']); ?>" class="btn-secondary block md:inline-block md:w-auto w-full text-center"><?= esc_html($ctaTwo['title']); ?></a>
                 <?php endif; ?>
             </div>
-            <p class="text-[1.041vw] !mb-[2.083vw]"><?= $heroExc ?></p>
+            <p class="text-[3.765vw] md:text-[1.041vw] !mb-[2.083vw]"><?= $heroExc ?></p>
         </div>
     </div>
 
