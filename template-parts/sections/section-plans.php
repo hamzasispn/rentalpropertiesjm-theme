580df0b44c086e3165980425aee2e7b620a704e4
hamza sispn <talha.sispn@gmail.com>
2026-02-05 03:35:03 +0500

    Create Plans Section Componenet

---
+++ template-parts/sections/section-plans.php
@@ -0,0 +1,20 @@
+<section id="packages" class="flex flex-col gap-8 w-[90%] md:pt-[3.125vw] pt-[13.882vw] mx-auto">
+    <div class="flex flex-col gap-3 mx-auto text-center mb-[2.708vw]">
+        <h5
+            class="text-[#1A1A1A] text-[7.6vw] md:text-[2.5vw] font-bold mb-[3.765vw] md:mb-[0.833vw] text-center leading-[1]">
+            Choose the Right Plan for Your
+            Property</h5>
+        <p
+            class="text-center w-full md:w-[36.438vw] text-[#1A1A1A] font-inter text-[3.765vw] md:text-[1.042vw] mx-auto">
+            List your property with
+            confidence. All our
+            plans come with verified listing approval, dashboard access, and the ability to boost your ad anytime.
+        </p>
+    </div>
+    <div
+        class="flex md:flex-nowrap flex-wrap gap-[3.765vw] md:gap-[0.833vw] space-x-4 md:space-y-0 space-y-4 justify-center">
+
+        <?php get_template_part('template-parts/component', 'plan-card'); ?>
+    </div>
+
+</section>
